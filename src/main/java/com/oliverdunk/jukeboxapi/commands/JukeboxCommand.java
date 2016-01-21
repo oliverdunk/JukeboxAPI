4316c2e4b860086bb83a1f425bcad0be31034370
Oliver Dunk <oliver@oliverdunk.com>
2016-01-21 20:32:25 +0000

    Allow command blocks and the console to run /jukebox

--- src/main/java/com/oliverdunk/jukeboxapi/commands/JukeboxCommand.java
+++ src/main/java/com/oliverdunk/jukeboxapi/commands/JukeboxCommand.java
@@ -15,28 +15,26 @@ public class JukeboxCommand implements CommandExecutor {
     private static final String PREFIX = ChatColor.GRAY + "[" + ChatColor.GREEN + "Jukebox" + ChatColor.GRAY + "] " + ChatColor.WHITE;
 
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-        if(!(commandSender instanceof Player)) return false;
-        Player player = (Player) commandSender;
-        if(!player.hasPermission("jukeboxapi.admin")){
-            player.sendMessage(PREFIX + "https://www.mcjukebox.net/?username=" + player.getName() + "&server=" + Jukebox.getInstance().getId());
+        if(!commandSender.hasPermission("jukeboxapi.admin")){
+            commandSender.sendMessage(PREFIX + "https://www.mcjukebox.net/?username=" + commandSender.getName() + "&server=" + Jukebox.getInstance().getId());
             return true;
         }
         if(args.length == 3){
             if(args[0].equalsIgnoreCase("music") | args[0].equalsIgnoreCase("sound")){
                 Player playFor = Bukkit.getPlayer(args[1]);
                 if(playFor == null){
-                    player.sendMessage(PREFIX + args[1] + " is not currently online.");
+                    commandSender.sendMessage(PREFIX + args[1] + " is not currently online.");
                     return true;
                 }
                 if(args[0].equalsIgnoreCase("music")) JukeboxAPI.play(playFor, args[2], ResourceType.MUSIC);
                 else JukeboxAPI.play(playFor, args[2], ResourceType.SOUND_EFFECT);
             }else{
-                player.sendMessage(PREFIX + "Usage: /jukeboxapi <music/sound> <player> <url>");
+                commandSender.sendMessage(PREFIX + "Usage: /jukeboxapi <music/sound> <player> <url>");
             }
         }else if(args.length == 0){
-            player.sendMessage(PREFIX + "https://www.mcjukebox.net/?username=" + player.getName() + "&server=" + Jukebox.getInstance().getId());
+            commandSender.sendMessage(PREFIX + "https://www.mcjukebox.net/?username=" + commandSender.getName() + "&server=" + Jukebox.getInstance().getId());
         }else{
-            player.sendMessage(PREFIX + "Usage: /jukeboxapi <music/sound> <player> <url>");
+            commandSender.sendMessage(PREFIX + "Usage: /jukeboxapi <music/sound> <player> <url>");
         }
         return true;
     }
